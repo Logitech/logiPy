687b5f8a08915a71f3b193f5ec05230daa7c01b0
Tom Lambert <tlambert@logitech.com>
2015-10-14 11:20:33 -0700

    updated the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "logipy",
-    version = "1.0.0",
+    version = "1.0.1",
     author = "Tom Lambert",
     author_email = "devtechsupport@logitech.com",
     description = ("A simple python wrapper for Logitech G's LED and Arx SDKs."),
