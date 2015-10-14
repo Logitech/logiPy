d2b9c8db667451eea9e8b346feef6c96f304f229
Tom Lambert <tlambert@logitech.com>
2015-10-14 10:50:44 -0700

    updated setup.py to the official release and converted the README.md to a README.rst for pypi

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 import os
 from setuptools import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
-
 setup(
     name = "logipy",
-    version = "0.0.1",
+    version = "1.0.0",
     author = "Tom Lambert",
     author_email = "devtechsupport@logitech.com",
     description = ("A simple python wrapper for Logitech G's LED and Arx SDKs."),
-    long_description = long_description,
+    long_description = open('README.rst').read(),
     keywords = ["logi", "logipy", "Logitech", "LogitechG", "LED", "Arx", "LGS", "Logitech Gaming Software"],
-    url = "http://packages.python.org/logipy",
+    url = "http://gaming.logitech.com/en-us/developers",
+	download_url = "https://github.com/Logitech/logiPy/tarball/master",
     packages=['logipy'],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Topic :: Software Development :: Libraries",
     ],
 )
