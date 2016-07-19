d9c9953373d1545e790e00c75587b82ae434b9c6
Tom Lambert <tlambert@logitech.com>
2016-07-19 15:15:35 -0700

    pypi version update

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ import os
 from setuptools import setup
 
 setup(
-    name = "logipy",
-    version = "1.0.1",
-    author = "Tom Lambert",
-    author_email = "devtechsupport@logitech.com",
-    description = ("A simple python wrapper for Logitech G's LED and Arx SDKs."),
+    name = 'logipy',
+    version = '1.1.0',
+    author = 'Tom Lambert',
+    author_email = 'devtechsupport@logitech.com',
+    description = ('A simple python wrapper for Logitech G\'s LED and Arx SDKs.'),
     long_description = open('README.rst').read(),
-    keywords = ["logi", "logipy", "Logitech", "LogitechG", "LED", "Arx", "LGS", "Logitech Gaming Software"],
-    url = "http://gaming.logitech.com/en-us/developers",
-	download_url = "https://github.com/Logitech/logiPy/tarball/master",
+    keywords = ['logi', 'logipy', 'Logitech', 'LogitechG', 'LED', 'Arx', 'LGS', 'Logitech Gaming Software'],
+    url = 'http://gaming.logitech.com/en-us/developers',
+	download_url = 'https://github.com/Logitech/logiPy/tarball/master',
     packages=['logipy'],
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Topic :: Software Development :: Libraries",
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development :: Libraries',
     ],
+    license='MIT',
 )
